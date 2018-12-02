ac57284f5242616d9ce6a317182a6bdc14eff195
Jason White <jwhite@dwtunnel.com>
2018-12-02 07:35:07 -0500

    Created navigation component

---
+++ web/client/src/app/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'grow-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.sass']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
