7370a84e1a2dcdae4a83fd6a0e8f3cec02d274cd
Jason White <jwhite@dwtunnel.com>
2018-12-02 18:14:01 -0500

    Created mock authentication guard

--- web/client/src/app/core/guards/authentication.guard.ts
+++ web/client/src/app/core/guards/authentication.guard.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { AuthenticationService } from '@core/services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
+
+  private isAuthenticated: boolean;
+
+  constructor(
+    private _authenticationService: AuthenticationService,
+    private _router: Router
+  ) {
+    this._authenticationService.authenticated
+      .subscribe(authenticated => this.isAuthenticated = authenticated);
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.isAuthenticated) return true;
+    this._router.navigate(['login']);
+    return false;
   }
+
 }
