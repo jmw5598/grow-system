bd14cebacaba8e1943a0a470d6a276b5c76a8cd8
Jason White <jwhite@dwtunnel.com>
2018-12-02 17:16:58 -0500

    Created dashboard module, temp dashboard component, and dashboard routing module.  Added lazy loaded routing to dashboard module in app.routing.

--- web/client/src/app/app.routing.ts
+++ web/client/src/app/app.routing.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardRoutingModule } from '@app/dashboard/dashboard.routing';
+
 import { LoginComponent } from '@app/login/login.component';
+
 const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: 'app/dashboard/dashboard.module#DashboardModule'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: '**',
     redirectTo: 'login',
     pathMatch: 'full'
   },
-  {
-    path: 'login',
-    component: LoginComponent
-  }
 ];
 
 
---
+++ web/client/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard.routing';
+
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ],
+  declarations: [DashboardComponent]
+})
+export class DashboardModule { }
