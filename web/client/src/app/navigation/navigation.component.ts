16886bbbd8b2451adcbae9dc5f70a54eae8b615b
Jason White <jwhite@dwtunnel.com>
2018-12-02 18:14:55 -0500

    Added logout function for mock logouts

--- web/client/src/app/navigation/navigation.component.ts
+++ web/client/src/app/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthenticationService } from '@core/services/authentication.service';
+
 @Component({
   selector: 'grow-navigation',
   templateUrl: './navigation.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _authenticationService: AuthenticationService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  logout() {
+    this._authenticationService.logout();
   }
 
 }
