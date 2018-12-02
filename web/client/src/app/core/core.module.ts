5bab4e7c44d4b8954aa1b894abac973da3f51ece
Jason White <jwhite@dwtunnel.com>
2018-12-02 17:57:45 -0500

    Created mock authentication service

--- web/client/src/app/core/core.module.ts
+++ web/client/src/app/core/core.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AuthenticationService } from './services/authentication.service';
+
 @NgModule({
+  declarations: [],
   imports: [
     CommonModule
   ],
-  declarations: []
+  providers: [
+    AuthenticationService
+  ]
 })
 export class CoreModule { }
--- web/client/src/app/core/services/authentication.service.ts
+++ web/client/src/app/core/services/authentication.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private isAuthenticated: boolean = false;
+  private authenticatedSource: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.isAuthenticated);
+  public authenticated = this.authenticatedSource.asObservable();
+
+  constructor() {}
+
+  login() {
+    this.isAuthenticated = true;
+    this.authenticatedSource.next(this.isAuthenticated);
+  }
+
+  logout() {
+    this.isAuthenticated = false;
+    this.authenticatedSource.next(this.isAuthenticated);
+  }
+
 }
