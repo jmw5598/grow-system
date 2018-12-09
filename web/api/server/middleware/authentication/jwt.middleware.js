2900fdb604d3b5bb438ea8d862942da85f18680b
Jason White <jwhite@dwtunnel.com>
2018-12-09 10:36:36 -0500

    added verify function to jwt.middleware.js to verify tokens

--- web/api/server/middleware/authentication/jwt.middleware.js
+++ web/api/server/middleware/authentication/jwt.middleware.js
@@ -6,6 +6,13 @@ class JwtMiddleware {
 
   constructor() {}
 
+  verify(req, res, next) {
+    const bearer = req.header('Authorization') || '';
+    const token = bearer.split(' ')[1];
+    const valid = JwtService.verify(token);
+    return valid ? next() : res.status(401).send({ error: 'Unauthorized' });
+  }
+
   hasRole(role) {
     return (req, res, next) => {
       const bearer = req.header('Authorization') || '';
