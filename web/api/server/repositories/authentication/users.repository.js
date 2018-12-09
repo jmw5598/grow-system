76bea620697ab87bddd92142cdb73dfbf394b837
Jason White <jwhite@dwtunnel.com>
2018-12-09 10:08:00 -0500

    created users repository to store/retrieve user details

---
+++ web/api/server/repositories/authentication/users.repository.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const { User, Role } = require('../../models');
+
+class UsersRepository {
+
+  constructor() {}
+
+  findAll() {
+    return User.findAll({
+      include: [{
+        model: Role,
+        as: 'roles',
+        attributes: ['role'],
+        through: { attributes: [] }
+      }],
+      attributes: {
+        exclude: ['id', 'password']
+      }
+    });
+  }
+
+  findById(id) {
+    return User.findByPk(id, {
+      include: [{
+        model: Role,
+        as: 'roles',
+        attributes: ['role'],
+        through: { attributes: [] }
+      }],
+      attributes: {
+        exclude: ['id', 'password']
+      }
+    });
+  }
+
+  findOne(username) {
+    return User.findOne({
+      where: { username: username },
+      include: [{
+        model: Role, as: 'roles', attributes: ['role'],
+        through: { attributes: [] }
+      }]
+    });
+  }
+
+  save(user) {
+    return User.build(user).save()
+      .then((user) => user)
+      .catch((error) => new Promise((resolve, reject) => reject(error)));
+  }
+
+  exists(id) {
+    return User.count({
+      where: { id: id }
+    });
+  }
+
+}
+
+module.exports = new UsersRepository();
---
+++ web/api/server/repositories/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const UsersRepository = require('./authentication/users.repository');
+
+module.exports = {
+  UsersRepository
+}
