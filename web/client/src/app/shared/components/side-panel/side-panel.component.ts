18d95bd96cb99d63e217e2ba643386dc07180b95
Jason White <jwhite@dwtunnel.com>
2018-12-24 16:19:23 -0500

    created structural layout components

---
+++ web/client/src/app/shared/components/side-panel/side-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'grow-side-panel',
+  templateUrl: './side-panel.component.html',
+  styleUrls: ['./side-panel.component.scss']
+})
+export class SidePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
