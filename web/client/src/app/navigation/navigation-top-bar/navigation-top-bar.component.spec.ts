55f38b9f7f7513d1bfb160441dfc17d3fed4f7dc
Jason White <jwhite@dwtunnel.com>
2018-12-24 16:20:45 -0500

    modified navigation component

--- web/client/src/app/navigation/navigation-top-bar/navigation-top-bar.component.spec.ts
+++ web/client/src/app/navigation/navigation-top-bar/navigation-top-bar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavigationComponent } from './navigation.component';
+import { NavigationTopBarComponent } from './navigation-top-bar.component';
 
-describe('NavigationComponent', () => {
-  let component: NavigationComponent;
-  let fixture: ComponentFixture<NavigationComponent>;
+describe('NavigationTopBarComponent', () => {
+  let component: NavigationTopBarComponent;
+  let fixture: ComponentFixture<NavigationTopBarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
+      declarations: [ NavigationTopBarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavigationComponent);
+    fixture = TestBed.createComponent(NavigationTopBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
