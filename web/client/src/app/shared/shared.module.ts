51bdd3594e23506fef17c2919a1a8b3fda9548ca
Jason White <jwhite@dwtunnel.com>
2018-12-24 16:19:46 -0500

    created structural layout components

--- web/client/src/app/shared/shared.module.ts
+++ web/client/src/app/shared/shared.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertMessageComponent } from './components/alert-message/alert-message.component';
+import { LayoutThreeColumnComponent } from './components/layout-three-column/layout-three-column.component';
+import { SidePanelComponent } from './components/side-panel/side-panel.component';
 
 @NgModule({
   declarations: [
-    AlertMessageComponent
+    AlertMessageComponent,
+    LayoutThreeColumnComponent,
+    SidePanelComponent
   ],
   exports: [
-    AlertMessageComponent
+    AlertMessageComponent,
+    LayoutThreeColumnComponent,
+    SidePanelComponent
   ],
   imports: [
     CommonModule
