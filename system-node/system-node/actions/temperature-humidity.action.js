80e0bf9be02d9fc2970d0f9df4f9330da352ff1e
Jason White <jmw5598@gmail.com>
2019-02-01 20:12:17 -0500

    added sensor logic to pull reading from sensor, stil have to figure out format for emitted messages

--- system-node/system-node/actions/temperature-humidity.action.js
+++ system-node/system-node/actions/temperature-humidity.action.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const DHTSensor = require('node-dht-sensor');
 const ComponentAction = require('./component.action');
+const SystemNodeContext = require('../system-node.context');
 const { MqttGateway } = require('../messaging');
+
 
 class TemperatureHumidityAction extends ComponentAction {
 
@@ -10,20 +13,27 @@ class TemperatureHumidityAction extends ComponentAction {
     this.temperature = 0;
     this.humidity = 0;
     this.preferences = config.preferences;
+    SystemNodeContext.configuration
+      .subscribe(configuration => this.systemConfiguration = configuration);
     this.start();
   }
 
   start() {
-    this.interval = setInterval(() => {
-      console.log('TemperatureHumidityAction::Alias::' + this.alias);
-      console.log('TemperatureHumidityAction::Taking readings');
-      console.log('TemperatureHumitidyAction::Publishing readings');
-    }, this.preferences.interval);
+    this.interval = setInterval(() => this.read(), this.preferences.interval);
   }
 
   stop() {
     if(this.interval)
       clearInterval(this.interval);
+  }
+
+  read() {
+    DHTSensor.read(22, this.pin, (error, temperature, humidity) => {
+      if(!error) {
+        const message = this.buildMessage(temperature, humidity);
+        MqttGateway.outbound('node/event/action', message);
+      }
+    });
   }
 
   setInterval(value) {
@@ -34,6 +44,26 @@ class TemperatureHumidityAction extends ComponentAction {
 
   setThreshold(value) {
     this.preferences.threshold = value;
+  }
+
+  buildMessage(temperature, humidity) {
+    // maybe create a class model for this?
+    // replace with configuration details when possible (node details)
+    return {
+      node: {
+        id: 123,
+        alias: "Alias",
+        component: {
+          id: this.id,
+          alias: this.alias,
+          type: this.type,
+          pin: this.pin,
+          preferences: this.preferences,
+          temperature: temperature,
+          humidity: humidity
+        }
+      }
+    }
   }
 
 }
