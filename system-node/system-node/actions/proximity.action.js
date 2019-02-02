1953ec3fc2e9910193e0a8ee492bc5df36b52779
Jason White <jmw5598@gmail.com>
2019-02-01 20:10:32 -0500

    created skeleton action for proxmity sensors

---
+++ system-node/system-node/actions/proximity.action.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const usonic = require('mmm-sonic');
+const ComponentAction = require('./component.action');
+
+class ProximityAction extends ComponentAction {
+
+  contructor() {
+    //https://www.npmjs.com/package/mmm-usonic-fixed
+  }
+
+  start() {
+
+  }
+
+  stop() {
+
+  }
+
+  setInterval(value) {
+
+  }
+
+  setThreshold(value) {
+
+  }
+
+}
