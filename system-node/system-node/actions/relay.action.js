d0ea6084ff4e5c30963bd7d779704eb57375b7c5
Jason White <jmw5598@gmail.com>
2019-02-10 17:15:26 -0500

    added onoff package for toggling relays, created relay.action logic

--- system-node/system-node/actions/relay.action.js
+++ system-node/system-node/actions/relay.action.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Gpio = require('onoff').Gpio;
 const ComponentAction = require('./component.action');
 const { MqttGateway } = require('../messaging');
 
@@ -9,13 +10,23 @@ class RelayAction extends ComponentAction {
     super(config.id, config.alias, config.type, config.pin);
     this.state = config.state;
     this.toggle('off');
+    this.relay = new Gpio(this.pin, 'out');
   }
 
   toggle(state) {
-    console.log('RelayAction::Alias::' + this.alias);
-    console.log('RelayAction::CurrentState::' + this.state);
-    this.state = state;
-    console.log('RelayAction::NewState::' + this.state);
+    switch(state.toLowerCase()) {
+      case 'on':
+        this.state = state.toLowerCase();
+        this.relay.writeSync(0);
+        break;
+      case 'off':
+        this.state = state.toLowerCase();
+        this.relay.writeSync(1);
+        break;
+      default:
+        console.log('[System-Node] RelayAction::State not recognized');
+        break;
+    }
   }
 
 }
