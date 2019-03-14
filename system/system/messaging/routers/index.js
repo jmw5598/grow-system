74f338417ecaa60f114d3a086b9cf1b963b30a10
Jason White <jmw5598@gmail.com>
2019-03-14 16:33:02 -0400

    updated index.js file for system routers (refactored/renamed)

--- system/system/messaging/routers/index.js
+++ system/system/messaging/routers/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const MqttRouter = require('./mqtt/mqtt.router');
-const SystemNodeActionEventRouter = require('./system-node/system-node-action-event.router');
-const SystemNodeActionRouter = require('./system-node/system-node-action.router');
-const SystemNodeRouter = require('./system-node/system-node.router');
-const SystemActionNodeRouter = require('./system/system-action-node.router');
 const SystemCommandRouter = require('./system/system-command.router');
+const SystemEventRouter = require('./system/system-event.router');
+const SystemNodeCommandRouter = require('./system-node/system-node-command.router');
+const SystemNodeEventRouter = require('./system-node/system-node-event.router');
+const SystemNodeRouter = require('./system-node/system-node.router');
 const SystemRouter = require('./system.router');
 
 module.exports = {
   MqttRouter,
-  SystemNodeActionRouter,
-  SystemNodeRouter,
-  SystemActionNodeRouter,
-  SystemActionRouter,
+  SystemCommandRouter,
+  SystemEventRouter,
+  SystemNodeCommandRouter,
+  SystemNodeEventROuter,
   SystemRouter
 }
