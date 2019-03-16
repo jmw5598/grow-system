0973246c05b30aea52537d4fa533db6b4d5b0bc4
Jason White <jmw5598@gmail.com>
2019-03-16 08:37:58 -0400

    fixed broken require import

--- system/system/messaging/routers/index.js
+++ system/system/messaging/routers/index.js
@@ -6,7 +6,7 @@ const SystemEventRouter = require('./system/system-event.router');
 const SystemNodeCommandRouter = require('./system-node/system-node-command.router');
 const SystemNodeEventRouter = require('./system-node/system-node-event.router');
 const SystemNodeRouter = require('./system-node/system-node.router');
-const SystemRouter = require('./system.router');
+const SystemRouter = require('./system/system.router');
 
 module.exports = {
   MqttRouter,
