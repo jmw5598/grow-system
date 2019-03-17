1030dfd804662493953cb59f45f03c19b1e74eed
Jason White <jmw5598@gmail.com>
2019-03-17 13:43:27 -0400

    updated system-node relay & temp-hum actions

--- system-node/system-node/actions/relay.action.js
+++ system-node/system-node/actions/relay.action.js
@@ -3,30 +3,37 @@
 const Gpio = require('onoff').Gpio;
 const ComponentAction = require('./component.action');
 const { MqttGateway } = require('../messaging');
+const { MqttMessage } = require('../messaging/models');
 
 class RelayAction extends ComponentAction {
 
   constructor(config) {
     super(config.id, config.alias, config.type, config.pin);
     this.state = config.state;
-    this.toggle('off');
     this.relay = new Gpio(this.pin, 'out');
+    this.toggle('off');
   }
 
   toggle(state) {
     switch(state.toLowerCase()) {
       case 'on':
         this.state = state.toLowerCase();
-        this.relay.writeSync(0);
+        this.relay.writeSync(0, this._notify(state));
         break;
       case 'off':
         this.state = state.toLowerCase();
-        this.relay.writeSync(1);
+        this.relay.writeSync(1, this._notify(state));
         break;
       default:
         console.log('[System-Node] RelayAction::State not recognized');
         break;
     }
+  }
+
+  _notify(state) {
+    console.log("[SystemNode] Sending updated value after relay action");
+    const message = new MqttMessage('system/system-node/event/relay', "message");
+    MqttGateway.outbound(message);
   }
 
 }
