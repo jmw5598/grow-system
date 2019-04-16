be434905a853d3e045f220d2ec2220bdeca5326f
Jason White <jmw5598@gmail.com>
2019-04-16 17:02:47 -0400

    refactored message routing

--- system-node/system-node/messaging/routers/index.js
+++ system-node/system-node/messaging/routers/index.js
@@ -1,9 +1,24 @@
 'use strict';
 
-const MqttRouter = require('./mqtt/mqtt.router');
+const MqttGateway = require('../gateways').MqttGateway;
+const MqttMessageRouter = require('./mqtt/mqtt-message.router');
+const SystemNodeMessageRouter = require('./system-node/system-node-message.router');
+const SystemNodeCommandMessageRouter = require('./system-node/system-node-command-message.router');
+
 const SystemNodeCommandRouter = require('./system-node/system-node-command.router');
+
+MqttGateway.routes.inbound.channel
+  .subcribe(message => MqttMessageRouter.route(message));
+
+MqttMessageRouter.routes.node.channel
+  .subscribe(message => SystemNodeMessageRouter.route(message));
+
+SystemNodeMessageRouter.routes.command.channel
+  .subscribe(message => SystemNodeCommandMessageRouter.route(message));
 
 module.exports = {
   MqttRouter,
+  SystemNodeMessageRouter,
+  SystemNodeCommandMessageRouter,
   SystemNodeCommandRouter
 }
