8e7cffa8d77a48047fbc88b971e271799e550a2b
Jason White <jmw5598@gmail.com>
2019-04-19 14:28:24 -0400

    created new application.context class to use observables for items added to the context.  This will replace system.context.js file

---
+++ system/system/application.context.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Rx = require('rxjs');
+
+class ApplicationContext {
+
+  constructor() {
+    this._context = {};
+  }
+
+  setItem(key, value) {
+    if (this._context.hasOwnProperty(key)) {
+      this._context[key].data = value;
+      this._context[key].source.next(value);
+    } else {
+      this._context[key] = { data: value };
+      this._context[key].source = new Rx.BehaviorSubject(this._context[key].data);
+      this._context[key].observable = this._context[key].source.asObservable();
+    }
+  }
+
+  getItem(key) {
+    if (this.context.hasOwnProperty(key))
+      return this._context[key].observable;
+    else
+      return Rx.EMPTY;
+  }
+
+}
+
+module.exports = new ApplicationContext();
