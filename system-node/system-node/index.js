178fdd273246331358b88c25b1631fdedce31d11
Jason White <jmw5598@gmail.com>
2019-04-20 08:15:36 -0400

    replace system-node.context with application.context

--- system-node/system-node/index.js
+++ system-node/system-node/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
+const ApplicationContext = require('./application.context');
 const { MqttGateway } = require('./messaging');
 const { MqttMessage } = require('./messaging/models');
-const { RelayAction, TemperatureHumidityAction } = require('./actions');
-const SystemNodeContext = require('./system-node.context');
 
 class SystemNode {
 
@@ -11,17 +10,15 @@ class SystemNode {
 
   setup(config) {
     this.config = config;
+    ApplicationContext.setItem('config', config);
+    MqttGateway.setup(config);
   }
 
   start() {
-    MqttGateway.init(this.config);
-    // TODO: Fix this so it works! (registering nodes on bootup and when system sends a reqeust to check in)
+    // TODO: Fix this so it works! (registering nodes on bootup and when system
+    // sends a reqeust to check in)
     const registration = new MqttMessage('system/system-node/event/register', this.config.node);
     MqttGateway.outbound(registration);
-    this.config.node.components.sensors.forEach(sensor =>
-        SystemNodeContext.register(new TemperatureHumidityAction(sensor)));
-    this.config.node.components.relays.forEach(relay =>
-        SystemNodeContext.register(new RelayAction(relay)));
   }
 
 }
