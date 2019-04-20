457d69aaf440612692c58944ab12082f4cbb960c
Jason White <jmw5598@gmail.com>
2019-04-20 08:17:32 -0400

    updated system-node messaging routers

--- system-node/system-node/messaging/routers/index.js
+++ system-node/system-node/messaging/routers/index.js
@@ -1,14 +1,8 @@
 'use strict';
 
-const MqttGateway = require('../gateways').MqttGateway;
 const MqttMessageRouter = require('./mqtt/mqtt-message.router');
 const SystemNodeMessageRouter = require('./system-node/system-node-message.router');
 const SystemNodeCommandMessageRouter = require('./system-node/system-node-command-message.router');
-
-const SystemNodeCommandRouter = require('./system-node/system-node-command.router');
-
-MqttGateway.routes.inbound.channel
-  .subcribe(message => MqttMessageRouter.route(message));
 
 MqttMessageRouter.routes.node.channel
   .subscribe(message => SystemNodeMessageRouter.route(message));
@@ -17,8 +11,7 @@ SystemNodeMessageRouter.routes.command.channel
   .subscribe(message => SystemNodeCommandMessageRouter.route(message));
 
 module.exports = {
-  MqttRouter,
+  MqttMessageRouter,
   SystemNodeMessageRouter,
-  SystemNodeCommandMessageRouter,
-  SystemNodeCommandRouter
+  SystemNodeCommandMessageRouter
 }
