e65c92682ea9e935451f44251f50712889f15dac
Jason White <jmw5598@gmail.com>
2019-05-11 19:56:42 -0400

    update system-node initializion and startup

--- system-node/system-node/index.js
+++ system-node/system-node/index.js
@@ -3,6 +3,7 @@
 const ApplicationContext = require('./application.context');
 const { MqttGateway } = require('./messaging');
 const { MqttMessage } = require('./messaging/models');
+const { RelayAction, TemperatureHumidityAction } = require('./actions');
 
 class SystemNode {
 
@@ -10,14 +11,30 @@ class SystemNode {
 
   setup(config) {
     this.config = config;
+    // TODO: Initialize node attribute in config with unique id if not already initialized.
+    // This if for new nodes only.
     ApplicationContext.setItem('config', config);
     MqttGateway.setup(config);
   }
 
   start() {
-    // TODO: Fix this so it works! (registering nodes on bootup and when system
-    // sends a reqeust to check in)
-    const registration = new MqttMessage('system/system-node/event/register', this.config.node);
+    const registration = new MqttMessage('system/node/register', this.config.node);
+    const actions = [];
+
+    // TODO: Not sure if this is the best place to initialize all the actions??
+    this.config.node.components.forEach(a => {
+      switch(a.type.model) {
+        case 'SRD05':
+          actions.push(new RelayAction(a));
+          break;
+        case 'DHT22':
+          actions.push(new TemperatureHumidityAction(a));
+          break;
+        default: break;
+      }
+    });
+
+    ApplicationContext.setItem('actions', actions);
     MqttGateway.outbound(registration);
   }
 
