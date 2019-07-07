ec2f41021fc4deee99c265447f677e7a79140d8c
Jason White <jmw5598@gmail.com>
2019-07-07 09:59:07 -0400

    udpated/fixed system-node-registration.service file, now register nodes properly

--- system/system/messaging/services/system-node-registration.service.js
+++ system/system/messaging/services/system-node-registration.service.js
@@ -1,22 +1,44 @@
 'use strict';
 
 const ApplicationContext = require('../../application.context');
-const { MqttGateway } = require('../gateways');
-const { SystemNodeMessageRouter } = require('../routers');
+const Logger = require('../../utilities').Logger;
+const MqttGateway = require('../gateways').MqttGateway;
+const SystemNodeMessageRouter = require('../routers').SystemNodeMessageRouter;
 
 class SystemNodeRegistrationService {
 
   constructor() {
+    this.logger = new Logger(this.constructor.name);
+    
     SystemNodeMessageRouter.routes.register.channel
       .subscribe(message => this.register(message));
+    
+    ApplicationContext.getItem('nodes')
+      .subscribe(nodes => this.nodes = nodes);
   }
 
   register(payload) {
-    console.log('payload:', payload);
-    /*
-      Do node registration logic here.  Register with ApplicationContext.
-      Send out new node registration to web.
-    */
+    this.logger.debug(`Registering new node: ${payload.message.name}`);
+    
+    if(this.nodes) {
+      const found = this.nodes.find(e => e.id === payload.message.id);
+      if (found) {
+        Object.assign(found, payload.message);
+        Object.assign(found.status, { state: 'online', timestamp: new Date() });
+      } else {
+        let node = payload.message;
+        node.status = { state: 'online', timestamp: new Date() };
+        this.nodes.push(paylaod);
+      }
+    }
+    else {
+      let node = payload.message;
+      node.status = { state: 'online', timestamp: new Date() };
+      this.nodes = [];
+      this.nodes.push(payload.message);
+    }
+
+    ApplicationContext.setItem('nodes', this.nodes);
   }
 
 }
