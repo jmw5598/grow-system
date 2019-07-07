1e07c436a5c247bbbd2f7659e4e10f730c2f6a03
Jason White <jmw5598@gmail.com>
2019-07-07 10:42:29 -0400

    added system-node-status.service to index barrel file

--- system-node/system-node/messaging/services/index.js
+++ system-node/system-node/messaging/services/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const RelayCommandService = require('./relay-command.service');
+const SystemNodeStatusService = require('./system-node-status.service');
 const TemperatureHumidityCommandService = require('./temperature-humidity-command.service');
 
 module.exports = {
   RelayCommandService,
+  SystemNodeStatusService,
   TemperatureHumidityCommandService
 }
