953ee7ce4ddbcaa9f64c2ff40ee0b61295c59c28
Jason White <jmw5598@gmail.com>
2019-07-13 13:12:35 -0400

    changed file formatting

--- web/api/server/controllers/system-node/component.controller.js
+++ web/api/server/controllers/system-node/component.controller.js
@@ -12,40 +12,30 @@ class ComponentController {
 
   create(req, res) {
     this.logger.debug(`Creating new component on node with id ${req.params.nodeId}`);
-    
     const message = this._generateMessage(
       req.params.nodeId, null, 'create', req.body.payload);
-    
     MqttGateway.outbound(message);
-    
     return res.status(200).send({ status: 'Ok', message: message });
   }
 
   update(req, res) {
     this.logger.debug(`Upading component on node with id ${req.params.nodeId}`);
-    
     const message = this._generateMessage(
       req.params.nodeId, req.params.componentId, 'update', req.body.payload);
-    
-      MqttGateway.outbound(message);
-    
+    MqttGateway.outbound(message);
     return res.status(200).send({ status: 'Ok', message: message });
   }
 
   delete(req, res) {
     this.logger.debug(`Deleting component on node with id ${req.params.nodeId}`);
-    
     const message = this._generateMessage(
       req.params.nodeId, req.params.componentId, 'delete', null);
-    
     MqttGateway.outbound(message);
-    
     return res.status(200).send({ status: 'Ok', message: message });
   }
 
   _generateMessage(node, component, command, payload) {
     const topic = `system/node/component/${command}`;
-    
     return new MqttMessage(topic, {
       command: command,
       component: { id: component },
