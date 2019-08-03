169c3386f908979327158dc8fbfcf0daae5d3191
Jason White <jmw5598@gmail.com>
2019-08-03 15:52:08 -0400

    add alert-message component, and a skeleton toggle-switch component

--- web/client/projects/libraries/gs-common/src/lib/gs-common.module.ts
+++ web/client/projects/libraries/gs-common/src/lib/gs-common.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { UiSwitchModule } from 'ngx-ui-switch';
+
+import { AlertMessageComponent } from './components/alert-message/alert-message.component';
+import { ToggleSwitchComponent } from './components/toggle-switch/toggle-switch.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
+  declarations: [
+    AlertMessageComponent,
+    ToggleSwitchComponent
   ],
-  exports: []
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    UiSwitchModule.forRoot({
+      size: 'small',
+      color: '#5CB85C',
+      switchColor: '#FFFFFF',
+      defaultBgColor: 'transparent',
+      defaultBoColor : '#CCC'
+    })
+  ],
+  exports: [
+    AlertMessageComponent,
+    ToggleSwitchComponent
+  ]
 })
 export class GsCommonModule { }
--- web/client/projects/libraries/gs-common/src/public-api.ts
+++ web/client/projects/libraries/gs-common/src/public-api.ts
@@ -2,4 +2,13 @@
  * Public API Surface of gs-common
  */
 
+// MODULES
 export * from './lib/gs-common.module';
+
+// COMPONENTS
+export * from './lib/components/toggle-switch/toggle-switch.component';
+export * from './lib/components/alert-message/alert-message.component';
+
+// MODELS
+export * from './lib/components/alert-message/alert-message.model';
+export * from './lib/components/alert-message/alert-type.enum';
