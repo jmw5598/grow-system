1b962582bce67a15e4043486ff737cccd213ebb9
Jason White <jmw5598@gmail.com>
2019-08-03 13:43:48 -0400

    created login page and login-form component

--- web/client/projects/applications/dashboard/src/app/features/login/login.module.ts
+++ web/client/projects/applications/dashboard/src/app/features/login/login.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 
 import { LoginComponent } from './pages/login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    FormsModule,
+    LoginRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
--- web/client/projects/applications/dashboard/src/app/features/login/pages/login/login.component.ts
+++ web/client/projects/applications/dashboard/src/app/features/login/pages/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService, Credentials, Token } from '@gs/core';
+
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'gs-login',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('loginForm')
+  loginForm: LoginFormComponent
 
-  ngOnInit() {
+  //public alert: AlertMessage;
+  public hasError: boolean = false;
+
+  constructor(
+    private _authenticationService: AuthenticationService,
+    private _router: Router
+  ) {
+    //this.alert = new AlertMessage("Invalid Username/Password", "", AlertType.WARNING);
+  }
+
+  ngOnInit() {}
+
+
+  onSubmit(credentials: Credentials) {
+    this._authenticationService.login(credentials)
+      .subscribe(
+        token => this.handleLoginSuccess(token),
+        error => this.handleLoginError(error)
+      );
+  }
+
+  private handleLoginSuccess(token: Token) {
+    this.hasError = false;
+    this.loginForm.form.reset();
+    this._router.navigate(['dashboard']);
+  }
+
+  private handleLoginError(error: Error) {
+    this.hasError = true;
+    //this.alert = new AlertMessage(error.error, "", AlertType.WARNING);
   }
 
 }
