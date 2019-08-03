4943ad9a1843bbed9c7f226883219e8eea73e0d9
Jason White <jmw5598@gmail.com>
2019-08-03 11:56:56 -0400

    create skeleton feature module with routing for login

---
+++ web/client/projects/applications/dashboard/src/app/features/login/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gs-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
