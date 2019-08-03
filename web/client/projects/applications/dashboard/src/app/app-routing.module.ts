c4c7187daa763ed0951d54ca8270229d375d52c9
Jason White <jmw5598@gmail.com>
2019-08-03 12:05:09 -0400

    updated app-routing and app.module with new feature modules

--- web/client/projects/applications/dashboard/src/app/app-routing.module.ts
+++ web/client/projects/applications/dashboard/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: './features/login/login.module#LoginModule'
+  },
+  {
+    path: 'system',
+    loadChildren: './features/system/system.module#SystemModule'
+  },
+  {
+    path: 'system-node',
+    loadChildren: './features/system-node/system-node.module#SystemNodeModule'
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- web/client/projects/applications/dashboard/src/app/app.module.ts
+++ web/client/projects/applications/dashboard/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { GsCommonModule } from '@gs/common';
+import { GsCoreModule } from '@gs/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GsCommonModule,
+    GsCoreModule.forRoot(environment),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
