e0db95e7dcf33cde6abf2fa0623961ab17c16c06
Jason White <jmw5598@gmail.com>
2019-09-08 10:16:53 -0400

    moved toaster component to gs-core library

--- web/client/projects/libraries/gs-core/src/lib/components/toaster/toast-message.model.ts
+++ web/client/projects/libraries/gs-core/src/lib/components/toaster/toast-message.model.ts
@@ -4,12 +4,14 @@ export class ToastMessage {
   public title: string;
   public message: string;
   public type: ToastType;
-  public duration: number
+  public duration: number;
+  public isShown: boolean;
 
   constructor(title: string, message: string, type: ToastType, duration: number) {
     this.title = title;
     this.message = message;
     this.type = type;
     this.duration = duration;
+    this.isShown = true;
   }
 }
