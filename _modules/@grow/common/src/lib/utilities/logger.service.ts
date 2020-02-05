9aae3171f3716393d76e71915fbc4d63c8d2d50d
Jason White <jmw5598@gmail.com>
2020-02-05 16:28:19 -0500

    WIP: added access modifiers to logger methods

--- _modules/@grow/common/src/lib/utilities/logger.service.ts
+++ _modules/@grow/common/src/lib/utilities/logger.service.ts
@@ -3,27 +3,27 @@ import { LogType } from './log-type.enum';
 export class Logger {
   constructor(public _class: string) {}
 
-  debug(message: string): void {
+  public debug(message: string): void {
     this._log(LogType.DEBUG, message);
   }
 
-  warn(message: string): void {
+  public warn(message: string): void {
     this._log(LogType.WARN, message);
   }
 
-  error(message: string): void {
+  public error(message: string): void {
     this._log(LogType.ERROR, message);
   }
 
-  danger(message: string): void {
+  public danger(message: string): void {
     this._log(LogType.DANGER, message);
   }
 
-  trace(message: string): void {
+  public trace(message: string): void {
     this._log(LogType.TRACE, message);
   }
 
-  _log(type: LogType, message: string): void {
+  private _log(type: LogType, message: string): void {
     const timestamp: Date = new Date();
     // tslint:disable-next-line:no-console
     console.log(`[${type}][${timestamp.toISOString()}][${this._class}] ${message}`);
