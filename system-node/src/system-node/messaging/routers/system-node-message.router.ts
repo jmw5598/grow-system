869bd28df4049334ba6e0d24442c1ae6faf173be
Jason White <jmw5598@gmail.com>
2020-02-08 10:18:27 -0500

    WIP: began converting system-node-message.router.ts

--- system-node/src/system-node/messaging/routers/system-node-message.router.ts
+++ system-node/src/system-node/messaging/routers/system-node-message.router.ts
@@ -4,11 +4,11 @@ export class SystemNodeMessageRouter extends AbstractMessageRouter {
   private static instance: SystemNodeMessageRouter;
 
   private constructor() {
-    super(
+    super([
       new MessageRoute('command', 'command'),
       new MessageRoute('component', 'component'),
       new MessageRoute('register', 'register')
-    );
+    ]);
   }
 
   public static getInstance(): SystemNodeMessageRouter {
