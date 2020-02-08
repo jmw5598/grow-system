eb54b6bd68960815a80ed05bd3f53d8d594848ce
Jason White <jmw5598@gmail.com>
2020-02-08 10:17:25 -0500

    WIP: created application.ts for root application, created constants file to hold constants to replace magic strings

---
+++ system-node/src/system-node/application.constants.ts
@@ -0,0 +1,11 @@
+export enum ApplicationContextKeys {
+  ACTIONS = 'actions',
+  CONFIG = 'config'
+}
+
+export enum ChannelSegments {
+  RELAY = 'relay',
+  COMPONENT = 'component',
+  REGISTER = 'register',
+  TEMPHUM = 'temphum'
+}
---
+++ system-node/src/system-node/application.ts
@@ -0,0 +1,35 @@
+import { ActionFactory } from './action.factory';
+import { Component, GlobalConfiguration, MqttMessage, MqttGateway, ApplicationContext, IRoutable, IMessageService } from '@grow/common';
+import { ApplicationContextKeys } from './application.constants';
+import { configureMessageRouters, configureMessageServices } from './messaging';
+
+export class SystemNode {
+  private _applicationContext: ApplicationContext;
+  private _gateway: MqttGateway;
+  private _messageRouting: IRoutable;
+  private _messageServices: IMessageService[];
+
+  constructor(private _config: GlobalConfiguration) {
+    this._applicationContext = ApplicationContext.getInstance();
+    this._applicationContext.setItem('config', this._config);
+    this._gateway = MqttGateway.getInstance();
+    this._messageRouting = configureMessageRouters();
+    this._messageServices = configureMessageServices();
+    this._gateway.setup(this._config.system.mqtt, this._messageRouting);
+  }
+
+  start() {
+    // @@@ TODO get rid of this magic string!
+    const registration = new MqttMessage('system/node/register', this._config.node);
+    const actions: any = [];
+
+    this._config.node.components.forEach((a: Component) => 
+       actions.push(ActionFactory.create(a.type.model, this._config.node, a)));
+    
+    // @@@ TODO get rid of this magic string??? Create enum????
+    // REPALCE EVERYWHERE
+    this._applicationContext.setItem(ApplicationContextKeys.ACTIONS, actions);
+    this._gateway.outbound(registration);
+  }
+
+}
