263c13bc30f03bac5b3a72718f137761a84ddf61
Jason White <jmw5598@gmail.com>
2020-02-09 07:43:55 -0500

    configured routers for system

--- system/src/app/messaging/routers/configure-routers.function.ts
+++ system/src/app/messaging/routers/configure-routers.function.ts
@@ -1,7 +1,38 @@
-import { IRoutable, MqttInboundMessageRouter } from '@grow/common';
+import { IRoutable, MqttInboundMessageRouter, MqttMessage } from '@grow/common';
+import { MqttMessageRouter } from './mqtt-message.router';
+import { SystemCommandMessageRouter } from './system-command-message.router';
+import { SystemEventMessageRouter } from './system-event-message.router';
+import { SystemMessageRouter } from './system-message.router';
+import { SystemNodeMessageRouter } from './system-node-message.router';
+import { SystemSchedulerMessageRouter } from './system-scheduler-message.router';
 
 export const configureMessageRouters: Function = (): IRoutable => {
   const mqttInboundMessageRouter: MqttInboundMessageRouter = MqttInboundMessageRouter.getInstance();
+  const mqttMessageRouter: MqttMessageRouter = MqttMessageRouter.getInstance();
+  const systemCommandMessageRouter: SystemCommandMessageRouter = SystemCommandMessageRouter.getInstance();
+  const systemEventMessageRouter: SystemEventMessageRouter = SystemEventMessageRouter.getInstance();
+  const systemMessageRouter: SystemMessageRouter = SystemMessageRouter.getInstance();
+  const systemNodeMessageRouter: SystemNodeMessageRouter = SystemNodeMessageRouter.getInstance();
+  const systemSchedulerMessageRouter: SystemSchedulerMessageRouter = SystemSchedulerMessageRouter.getInstance();
 
+  // @@@ TODO get rid fo these magic string
+  // Do the same in system-node routers function
+  mqttInboundMessageRouter.getChannel('inbound')
+    .subscribe((message: MqttMessage) => mqttMessageRouter.routeMessage(message));
+
+  mqttMessageRouter.getChannel('system')
+    .subscribe((message: MqttMessage) => systemMessageRouter.routeMessage(message));
+
+  mqttMessageRouter.getChannel('node')
+    .subscribe((message: MqttMessage) => systemNodeMessageRouter.routeMessage(message));
+
+  systemMessageRouter.getChannel('command')
+    .subscribe((message: MqttMessage) => systemCommandMessageRouter.routeMessage(message));
+
+  systemMessageRouter.getChannel('event')
+    .subscribe((message: MqttMessage) => systemEventMessageRouter.routeMessage(message));
+
+  
+  
   return mqttInboundMessageRouter;
 };
