0e66a8949e810ce09cdd44bf285418555bbfba63
Jason White <jmw5598@gmail.com>
2020-02-10 18:10:00 -0500

    updated index entry to read configuration properly

--- system-node/src/index.ts
+++ system-node/src/index.ts
@@ -1,11 +1,13 @@
 import * as fs from 'fs' ;
+import * as path from 'path';
 import { GlobalConfiguration } from '@grow/common';
 
 import { mapIdsToSubscriptions } from './config/map-id-to-subscriptions.utility';
 import { SystemNode } from './app';
 
-const config: GlobalConfiguration = mapIdsToSubscriptions(JSON.parse(
-    fs.readFileSync('config/system-node.json', 'utf8')) as GlobalConfiguration);
+const filePath: string = path.resolve(__dirname, './config/configuration.json');
+const config: GlobalConfiguration = mapIdsToSubscriptions(
+    JSON.parse(fs.readFileSync(filePath, 'utf8')) as GlobalConfiguration);
 
 const systemNode: SystemNode = new SystemNode(config);
 systemNode.start();
