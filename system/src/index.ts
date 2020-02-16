ea2880fe15c43e1fb2e726d86bd821d5980cf709
Jason White <jmw5598@gmail.com>
2020-02-16 12:03:51 -0500

    moved system configuration.json file to root of application

--- system/src/index.ts
+++ system/src/index.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { GlobalConfiguration } from '@grow/common';
 import { System } from './app';
 
-const filePath: string = path.resolve(__dirname, './config/configuration.json');
+const filePath: string = path.resolve(__dirname, '../configuration.json');
 const config: GlobalConfiguration = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
 const system: System = new System(config);
