a90c7346ab8f3f4d3fb1a451eb842cc05576cae1
Jason White <jmw5598@gmail.com>
2020-03-03 19:16:04 -0500

    create application.constants.ts file

---
+++ web/api/src/app/application.contants.ts
@@ -0,0 +1,22 @@
+export enum ApplicationContextKeys {
+  ACTIONS = 'actions',
+  CONFIG = 'config',
+}
+
+export enum ChannelSegments {
+  COMMAND = 'command',
+  COMPONENT = 'component',
+  EVENT = 'event',
+  HUMIDITY = 'humidity',
+  INBOUND = 'inbound',
+  MOISTURE = 'moisture',
+  NODE = 'node',
+  NOTIFICATION = 'notification',
+  PROXIMITY = 'proximity',
+  REGISTER = 'register',
+  RELAY = 'relay',
+  STATUS = 'status',
+  SYSTEM = 'system',
+  TEMPERATURE = 'temperature',
+  TEMPHUM = 'temphum',
+}
