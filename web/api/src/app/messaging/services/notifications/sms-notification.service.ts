f27ede4863246ffd3117a25a8810b67f55c6257e
Jason White <jmw5598@gmail.com>
2020-03-22 08:22:31 -0400

    added twilio dep

--- web/api/src/app/messaging/services/notifications/sms-notification.service.ts
+++ web/api/src/app/messaging/services/notifications/sms-notification.service.ts
@@ -1,11 +1,29 @@
 import { MqttMessage, IMessageService, IPubSubChannel } from '@grow/common';
 
+import twilio, { Twilio } from 'twilio';
+
 export class SmsNotificationService implements IMessageService {
+  private readonly _twilioClient: Twilio;
+
   constructor(channel: IPubSubChannel) {
+    const sid: string | undefined = process.env.TWILIO_ACCOUNT_SID;
+    const tok: string | undefined = process.env.TWILIO_AUTH_TOKEN;
+    this._twilioClient = twilio(sid, tok);
     channel.receivedMessage().subscribe((message: MqttMessage) => this.processMessage(message));
   }
 
   public processMessage(message: MqttMessage): void {
-    console.log('[API] Sending SMS notificaiton');
+    this._twilioClient.messages;
+
+    const from = process.env.TWILIO_FROM || '';
+    const to = process.env.TWILIO_TO || '';
+
+    const msgData: any = {
+      from: from,
+      to: to,
+      body: message.message,
+    };
+
+    // @@@ TODO Send sms message.
   }
 }
